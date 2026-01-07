159229c80df12c80a71e4a24a57ac6cbf7ae7fd9
manuu <manuel.gasser3@gmx.ch>
2026-01-07 12:44:17 +0100

    rem db, added examples

--- app/app.py
+++ app/app.py
@@ -1,72 +1,68 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv # Lädt .env Datei
+from services import math_service
 from config import DevelopmentConfig, ProductionConfig
-from db import close_db, init_db
-from services import auth_service
-from repository import postgresql_repo as user_repo
 
+# Definieren einer Variable, die die aktuelle Datei zum Zentrum
+# der Anwendung macht.
+app = Flask(__name__)
+
+"""
+Festlegen einer Route für die Homepage. Der String in den Klammern
+bildet das URL-Muster ab, unter dem der folgende Code ausgeführt
+werden soll.
+z.B.
+* @app.route('/')    -> http://127.0.0.1:5000/
+* @app.route('/home') -> http://127.0.0.1:5000/home
+"""
+
+#-------------------------------
+#Vorbereitungen
 # 1. .env laden (macht lokal Variablen verfügbar, auf Render passiert nichts)
 load_dotenv()
 
-app = Flask(__name__)
 
 # 2. Config wählen
 if os.environ.get('FLASK_ENV') == 'development':
     app.config.from_object(DevelopmentConfig)
 else:
     app.config.from_object(ProductionConfig)
+#-------------------------------
 
-app.teardown_appcontext(close_db)
+# mock data
+languages = [
+    {"name": "Python", "creator": "Guido van Rossum", "year": 1991},
+    {"name": "JavaScript", "creator": "Brendan Eich", "year": 1995},
+    {"name": "Java", "creator": "James Gosling", "year": 1995},
+    {"name": "C#", "creator": "Microsoft", "year": 2000},
+    {"name": "Ruby", "creator": "Yukihiro Matsumoto", "year": 1995},
+]
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    print(math_service.add(1.0, 2.0))
+    app.logger.info("Rendering home page")
+    return render_template("home.html")
 
-@app.route('/init-db')
-def initialize():
-    try:
-        init_db()
-        return "Datenbank Tabelle 'users' erstellt."
-    except Exception as e:
-        return f"Fehler: {e}"
+# Route with a parameter in the URL
+@app.route('/result/', defaults={'name': 'Guest'})
+@app.route('/result/<name>')
+def result(name) -> str:
+    app.logger.info(f"Showing result for {name}")
+    return render_template("result.html", name=name)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        action = request.form['action']
-        username = request.form['username']
-        password = request.form['password']
+@app.route("/about")
+def about() -> str:
+    return render_template("about.html", languages=languages)
 
-        if action == 'register':
-            if user_repo.create_user(username, password):
-                flash('Registriert! Jetzt einloggen.')
-            else:
-                flash('Username vergeben.')
-            
-        elif action == 'login':
-            user_id = auth_service.authenticate(username, password)
-            if user_id:
-                session['user_id'] = user_id
-                return redirect(url_for('profile'))
-            else:
-                flash('Falsche Daten.')
-
-    return render_template('login.html')
-
-@app.route('/profile')
-def profile():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    
-    user = user_repo.get_user_by_id(session['user_id'])
-    return render_template('profile.html', username=user[1], user_id=user[0])
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('home'))
+@app.route("/submit", methods=["POST"])
+def submit():
+    app.logger.info("Form submitted")
+    # Access form data (request body parameters)
+    name = request.form.get("name")
+    # Redirect to a new URL, passing a parameter in the URL
+    return redirect(url_for("result", name=name))
 
 if __name__ == '__main__':
-    # Lokal starten
     app.run(port=5000)
--- app/services/envvar_service.py
+++ app/services/envvar_service.py
@@ -1,8 +1,7 @@
 import os
 
 def get_env_var(var_name: str) -> str:
-    """Get an environment variable or raise an error if not found."""
     value = os.getenv(var_name)
     if value is None:
-        raise EnvironmentError(f"Environment variable {var_name} not found.")
+        raise EnvironmentError(f"Environment Variable {var_name} not found.")
     return value
