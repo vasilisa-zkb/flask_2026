6d5fb1f6b1ea59157ff394180866371741f8628d
Matteo Cocetrone <matteococetrone@gmail.com>
2026-02-17 16:11:18 +0100

    Refactor init_app function by removing redundant indentation

--- app/db.py
+++ app/db.py
@@ -29,10 +29,6 @@ def init_db_command():
     init_db()
     click.echo('Datenbank initialisiert.')
 
-    def init_app(app):
-        app.teardown_appcontext(close_db)
-        app.cli.add_command(init_db_command)
-
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
